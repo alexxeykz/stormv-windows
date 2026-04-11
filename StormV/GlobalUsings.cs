7966f6358f8c227f98533511800cec1d3205096d
alexxeykz <alexxeykz@users.noreply.github.com>
2026-04-11 21:52:05 +0200

    Add System.Net.Http to global usings

--- StormV/GlobalUsings.cs
+++ StormV/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using System.Diagnostics;
 global using System.IO;
 global using System.Linq;
 global using System.Net;
+global using System.Net.Http;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
